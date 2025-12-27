1c9fb8022c016c13b4dd26e6693e717e5fc2d2be
AfifaSiddiqui005 <afifa.siddiqui04@gmail.com>
2025-12-28 00:41:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,4 +26,13 @@ viewMoreBtn.addEventListener("click", function() {
         card.classList.remove("hidden");
     });
     viewMoreBtn.style.display = "none";
+
+});
+
+//BUTTON FOR NAV MENU
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const navMenu = document.getElementById('nav-menu');
+
+hamburgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
 });
